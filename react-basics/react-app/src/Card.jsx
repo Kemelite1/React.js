9de4a46c840aa86c7a7ddb5913ed41206be9f4a9
Kemelite1 <osikemelite42@gmail.com>
2024-02-18 20:10:04 +0100

    creating card components

---
+++ react-basics/react-app/src/Card.jsx
@@ -0,0 +1,10 @@
+function Card(){
+    return (
+        <div className="card">
+            <img className="card-img" src="https://img.freepik.com/free-photo/pretty-hair-keeps-hand-raised-smiles-gently-looks-directly-listens-attentively-interlocutor-wears-stylish-velvet-jacket_273609-46286.jpg?w=740&t=st=1708282423~exp=1708283023~hmac=4d4b5a7aaaeaea1fd71dfef93d3fdc9806d079cf963c72b7d25c4e79477b3967" alt="Travel girl image" />
+            <h2 className="card-title">TravelGirl</h2>
+            <p className="card-description">I am a travel entrepreneur</p>
+        </div>
+    )
+}
+export default Card;
