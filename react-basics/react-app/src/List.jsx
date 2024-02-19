9e9707f85c189a0517ae0bccefa1ebca55261277
Kemelite1 <osikemelite42@gmail.com>
2024-02-19 22:24:00 +0100

    Adding a unique key property

--- react-basics/react-app/src/List.jsx
+++ react-basics/react-app/src/List.jsx
@@ -1,8 +1,12 @@
 
 function List() {
-  const states = ['Lagos', 'Imo', 'Bayelsa', 'Port Harcourt', 'Calabar']
+  const states = [{name: 'Lagos', capital: 'Ikeja'},
+   {name: 'Imo', capital: 'Owerri'},
+    {name: 'Bayelsa', capital: 'Yenagoa'},
+     {name: 'Rivers', capital: 'Port Harcourt'},
+      {name: 'Cross River', capital: 'Calabar'}];
 
-  const items = states.map(state => <li>{state}</li>)
+  const items = states.map(state => <li key={state.name}> {state.name}</li>)
   return (
     <ul>
     {items}
