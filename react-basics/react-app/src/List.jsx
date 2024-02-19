b975398ebcf6e02e4596dedd649fa66b4c7348f4
Kemelite1 <osikemelite42@gmail.com>
2024-02-20 00:05:21 +0100

    Sorting states alphabetically

--- react-basics/react-app/src/List.jsx
+++ react-basics/react-app/src/List.jsx
@@ -6,6 +6,9 @@ function List() {
      {id: 3, name: 'Rivers', capital: 'Port Harcourt'},
       {id: 4, name: 'Cross River', capital: 'Calabar'}];
 
+      // sorting alphabetically
+      states.sort((a, b) => a.name.localeCompare(b.name));
+      
   const items = states.map(state => <li key={state.id}> {state.name}: &nbsp; <b>{state.capital}</b></li>)
   return (
     <ul>
