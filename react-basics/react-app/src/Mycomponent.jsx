0ad8b5b4f51e288afbea0aec90c455b20af06d06
Kemelite1 <osikemelite42@gmail.com>
2024-02-20 22:11:01 +0100

    useState hook

--- react-basics/react-app/src/Mycomponent.jsx
+++ react-basics/react-app/src/Mycomponent.jsx
@@ -2,16 +2,27 @@ import React, {useState} from "react";
 
 function Mycomponent() {
   
-    const [name, setName] = useState();
+    const [count, setCount] = useState(0);
 
-    const updateName = () =>{
-        setName('Bovi')
+    const increment = () => {
+        setCount(count + 1)
     }
 
+    const decrement = () => {
+        setCount(count - 1)
+    }
+
+    const reset = () => {
+        setCount(0)
+    }
+
+
     return (
-        <div>
-            <p>Name: {name}</p>
-            <button onClick={updateName}>set Name</button>
+        <div className="counter">
+            <p className="counter-display"> {count}</p>
+            <button className="btn" onClick={increment}>Add</button>
+            <button className="btn" onClick={reset}>Reset</button>
+            <button className="btn" onClick={decrement}>Subtract</button>
         </div>
     )
 }
