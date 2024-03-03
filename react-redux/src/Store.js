5c7add28aa5715537415bd1448a4d03d54c576a7
Kemelite1 <osikemelite42@gmail.com>
2024-03-03 11:09:15 +0100

    Created initialState for the account and customer

---
+++ react-redux/src/Store.js
@@ -0,0 +1,13 @@
+
+const initialStateAccount = {
+    balance: 0,
+    loan: 0,
+    loanPurpose: "",
+};
+
+const initialStateCustomer = {
+    fullName: '',
+    nationID: '',
+    createdAt: '',
+
+};
