60250712efca44d72ea08e7340d67cffeaf80346
Kemelite1 <osikemelite42@gmail.com>
2024-03-03 14:17:23 +0100

    importing the account and customer slices

--- react-redux/src/Store.js
+++ react-redux/src/Store.js
@@ -1,104 +1,12 @@
 import { combineReducers, createStore } from 'redux';
-
-const initialStateAccount = {
-    balance: 0,
-    loan: 0,
-    loanPurpose: "",
-};
-
-const initialStateCustomer = {
-    fullName: '',
-    nationID: '',
-    createdAt: '',
-
-};
-
-function accountReducer(state = initialStateAccount, action){
-    switch(action.type){
-        case "account/deposit":
-            return {...state, balance: state.balance + action.payload};
-        case "account/withdraw":
-            return {...state, balance: state.balance - action.payload};
-            case "account/requestLoan":
-                if(state.loan > 0) return state;
-                return {...state,
-                    loan: action.payload.amount,
-                     loanPurpose: action.payload.purpose,
-                    balance: state.balance + action.payload.amount};
-                case "account/payLoan":
-                    return {...state,
-                         loan: 0,
-                          loanPurpose: "",
-                           balance: state.balance - state.loan}
-            default:
-                return state;
-    }
-}
-
-function customerReducer(state = initialStateCustomer, action){
-    switch(action.type){
-        case 'customer/createCustomer':
-        return {...state,
-             fullName: action.payload.fullName,
-              nationID: action.payload.nationID,
-               createdAt: action.payload.createdAt,
-            };
-            case 'customer/updateName':
-                return {...state, fullName: action.payload};
-
-                default:
-                    return state;
-    }
-}
-
+import accountReducer from './features/accounts/AccountSlice';
+import customerReducer from './features/customers/CustomerSlice';
 
 const rootReducer = combineReducers({
     account: accountReducer,
     customer: customerReducer,
-})
+});
 
 const store = createStore(rootReducer);
 
-// store.dispatch({ type: "account/deposit", payload: 500});
-// store.dispatch({ type: "account/requestLoan", payload: { amount: 2000, purpose: "Buy a house"}});
-// console.log(store.getState());
-
-// Action creators for account
-function deposit(amount){
-    return { type: "account/deposit", payload: amount}
-}
-
-function withdraw(amount){
-    return { type: "account/withdraw", payload: amount}
-}
-
-function requestLoan(amount, purpose){
-    return { type: "account/requestLoan", payload: { amount, purpose}}
-}
-
-function payLoan(){
-    return { type: "account/payLoan"}
-}
-
-store.dispatch(deposit(5000))
-store.dispatch(withdraw(2000))
-store.dispatch(requestLoan(1000, "To buy a house"))
-store.dispatch(payLoan())
-console.log(store.getState());
-
-
-//Action creators for customer
-function createCustomer(fullName, nationID){
-    return {
-         type: "customer/createCustomer",
-          payload: { fullName, nationID, createdAt: new Date().toISOString() }
-        };
-}
-
-function updateName(fullName){
-    return { type: "account/updatedName", payload: fullName}
-}
-
-//dispatching actions for the customer
-store.dispatch(createCustomer('Bovi Kay', '2254678'));
-console.log(store.getState());
+export default store;
