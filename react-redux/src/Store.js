d8b570fec16b934ce5dee9252d71ae97578c8d93
Kemelite1 <osikemelite42@gmail.com>
2024-03-03 11:11:48 +0100

    created Reducers for the account and customer

--- react-redux/src/Store.js
+++ react-redux/src/Store.js
@@ -11,3 +11,41 @@ const initialStateCustomer = {
     createdAt: '',
 
 };
+
+function accountReducer(state = initialStateAccount, action){
+    switch(action.type){
+        case "account/deposit":
+            return {...state, balance: state.balance + action.payload};
+        case "account/withdraw":
+            return {...state, balance: state.balance - action.payload};
+            case "account/requestLoan":
+                if(state.loan > 0) return state;
+                return {...state,
+                    loan: action.payload.amount,
+                     loanPurpose: action.payload.purpose,
+                    balance: state.balance + action.payload.amount};
+                case "account/payLoan":
+                    return {...state,
+                         loan: 0,
+                          loanPurpose: "",
+                           balance: state.balance - state.loan}
+            default:
+                return state;
+    }
+}
+
+function customerReducer(state = initialStateCustomer, action){
+    switch(action.type){
+        case 'customer/createCustomer':
+        return {...state,
+             fullName: action.payload.fullName,
+              nationID: action.payload.nationID,
+               createdAt: action.payload.createdAt,
+            };
+            case 'customer/updateName':
+                return {...state, fullName: action.payload};
+
+                default:
+                    return state;
+    }
+}
