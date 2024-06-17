1f026b1936de938ded2efc171e4560e4c3da9f57
kemelite1 <osikemelite42@gmail.com>
2024-06-17 19:32:35 +0100

    Jobs components - JobOpening.jsx (component) - hardcode data - jobs.json (data) - array of objects - App.jsx (main component) - render JobOpening component

--- joblisting/src/App.jsx
+++ joblisting/src/App.jsx
@@ -1,5 +1,6 @@
 import Hero from "./components/Hero"
 import HomeCard from "./components/HomeCard"
+import JobOpening from "./components/JobOpening"
 import Navbar from "./components/Navbar"
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
       <Navbar />
       <Hero/>
       <HomeCard />
+      <JobOpening />
     </div>
   )
 }
