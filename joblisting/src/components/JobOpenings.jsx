bc81206e3c2621da9538502b98af2d1f8defa651
kemelite1 <osikemelite42@gmail.com>
2024-06-18 17:36:18 +0100

    Display first three jobs only - Used slice method to display first three jobs only - Used map method to display the jobs

--- joblisting/src/components/JobOpenings.jsx
+++ joblisting/src/components/JobOpenings.jsx
@@ -2,18 +2,19 @@ import jobs from '../jobs.json'
 import JobOpening from './JobOpening';
 
 const JobOpenings = () => {
-    console.log(jobs);
+    const recentJobs = jobs.slice(0, 3);
+    
   return (
     <section className="bg-green-200 py-10 px-4">
         <div className="m-auto lg:container container-xl">
             <h2 className="text-stone-500 text-3xl font-bold text-center mb-6">Browse Jobs</h2>
             <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
 
-                {jobs.map( (job) => (
+                {recentJobs.map( (job) => (
                     <JobOpening key={job.id} job={job}/>
 
                 ) )}
-                
+
             </div>
         </div>
     </section>
