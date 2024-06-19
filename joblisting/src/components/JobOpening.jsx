de32756dcd1e36ae6754194cb13f9da81b3f08d3
kemelite1 <osikemelite42@gmail.com>
2024-06-19 14:41:52 +0100

    update the JobOpening component to use the FaMapMarker icon import react-icon from 'react-icons'; import { FaMapMarker } from 'react-icons/fa';

--- joblisting/src/components/JobOpening.jsx
+++ joblisting/src/components/JobOpening.jsx
@@ -1,6 +1,6 @@
 import { PropTypes } from 'prop-types';
 import { useState } from 'react';
-
+import { FaMapMarker } from 'react-icons/fa';
 
 const JobOpening = ( { job } ) => {
 
@@ -30,7 +30,7 @@ const JobOpening = ( { job } ) => {
 
                         <div className="mb-4 flex flex-col lg:flex-row justify-between">
                             <div className="mb-3 text-green-500">
-                                <i className="fa-solid fa-location-dot text-lg"></i>
+                                <FaMapMarker className='inline mb-1 text-lg'/>
                                 {job.location}</div>
                             <a href={`/job/${job.id}`} className="py-2 px-4 bg-black hover:bg-gray-700 text-white text-sm rounded-lg h-[36px] text-center">Read More</a>
                         </div>
