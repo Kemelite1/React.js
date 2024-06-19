7dab6929048af2fd385c4dcc0266f18f25155027
kemelite1 <osikemelite42@gmail.com>
2024-06-19 19:22:36 +0100

    Create layout folder in src Create MainLayout.jsx in layout folder use outlet in MainLayout.jsx import HomeCard, JobOpenings and ViewAllJobs in HomePage.jsx

--- joblisting/src/pages/HomePage.jsx
+++ joblisting/src/pages/HomePage.jsx
@@ -1,9 +1,15 @@
 import Hero from '../components/Hero'
+import HomeCard from '../components/HomeCard'
+import JobOpenings from '../components/JobOpenings'
+import ViewAllJobs from '../components/ViewAllJobs'
 
 const HomePage = () => {
   return (
     <>
       <Hero />
+      <HomeCard />
+      <JobOpenings />
+      <ViewAllJobs />
     </>
   )
 }
