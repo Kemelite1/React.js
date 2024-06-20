1ee4cdc9b466789dff5f396febeb6e55c7863b0f
kemelite1 <osikemelite42@gmail.com>
2024-06-20 18:22:36 +0100

    Updated

--- joblisting/src/components/JobOpening.jsx
+++ joblisting/src/components/JobOpening.jsx
@@ -33,7 +33,7 @@ const JobOpening = ( { job } ) => {
                             <div className="mb-3 text-green-500">
                                 <FaMapMarker className='inline mb-1 mr-1 text-lg'/>
                                 {job.location}</div>
-                            <Link to={`/job/${job.id}`} className="py-2 px-4 bg-black hover:bg-gray-700 text-white text-sm rounded-lg h-[36px] text-center">Read More</Link>
+                            <Link to={`/jobs/${job.id}`} className="py-2 px-4 bg-black hover:bg-gray-700 text-white text-sm rounded-lg h-[36px] text-center">Read More</Link>
                         </div>
 
                     </div>
