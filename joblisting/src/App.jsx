0663ead1d2b4e6df8584a7cb83ed6a56ca8aec11
kemelite1 <osikemelite42@gmail.com>
2024-06-20 13:57:20 +0100

    Update PageNotFound.jsx

--- joblisting/src/App.jsx
+++ joblisting/src/App.jsx
@@ -10,7 +10,6 @@ const router = createBrowserRouter(
   createRoutesFromElements(
   <Route path="/" element={<MainLayout />}>
     <Route index element={<HomePage />} />
-    
     <Route path="/jobs" element={<JobsPage />} />
     <Route path="*" element={<PageNotFound />} />
     </Route>
--- joblisting/src/pages/PageNotFound.jsx
+++ joblisting/src/pages/PageNotFound.jsx
@@ -8,7 +8,7 @@ const PageNotFound = () => {
         <FaExclamationTriangle className='text-red-500 text-6xl mb-4' />
         <h1 className='mb-4 text-6xl font-bold'>404 Not Found</h1>
         <p className='mb-5 text-xl'>Sorry, the page you are looking for does not exist</p>
-        <Link to='/' className='bg-indigo-700 hover:bg-indigo-900 text-white py-2 px-3 mt-4 rounded-lg'>Go Back</Link>
+        <Link to='/' className='bg-black hover:bg-gray-900 text-white py-2 px-3 mt-4 rounded-lg'>Go Back</Link>
         
         </section>
   )
