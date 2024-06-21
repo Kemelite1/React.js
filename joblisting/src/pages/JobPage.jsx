a774912a097b30450bec79605559711813f672fd
kemelite1 <osikemelite42@gmail.com>
2024-06-21 18:27:38 +0100

    Replace hard coded data with dynamic data

--- joblisting/src/pages/JobPage.jsx
+++ joblisting/src/pages/JobPage.jsx
@@ -24,22 +24,20 @@ const JobPage = () => {
 
               <main>
                 <div className='bg-white text-center p-6 rounded-lg md:text-left shadow-md'>
-                  <div className='mb-4 text-gray-500'>Full-Time</div>
-                  <h1 className='mb-4 text-3xl font-bold'>Senior Front-end Developer</h1>
+                  <div className='mb-4 text-gray-500'>{job.type}</div>
+                  <h1 className='mb-4 text-3xl font-bold'>{job.title}</h1>
 
                   <div className='flex justify-center align-middle md:justify-start text-gray-500 mb-4'>
                     <FaLocationDot className='mr-2 text-green-500 text-lg' />
-                    <p className='text-green-500'>Lagos, NG</p>
+                    <p className='text-green-500'>{job.location}</p>
                   </div>
                 </div>
 
                 <div className='bg-white p-6 mt-6 rounded-lg shadow-md'>
                   <h3 className='text-gray-700 font-bold text-lg mb-6'>Job Description</h3>
-                  <p className='mb-4'>We are seeking a senior Front-end developer to join our team.
-                  The ideal candidate will have strong skills in HTML, CSS, and JavaScript, with experience working with modern JavaScript frameworks such as React or Angular.
-                  </p> 
+                  <p className='mb-4'>{job.description}</p> 
                   <h3 className='text-gray-700 font-bold text-lg mb-2'>Salary</h3>
-                  <p className='mb-4'>$80k - $100K / Year</p>
+                  <p className='mb-4'>{job.salary} / Year</p>
                 </div>
 
 
@@ -49,18 +47,18 @@ const JobPage = () => {
               <aside>
                 <div className='bg-white p-6 rounded-lg shadow-md'>
                   <h3 className='mb-6 text-xl font-bold'>Company Info</h3>
-                  <h2 className='text-2xl'>Teencoders Techhub</h2>
-                  <p className='my-2'>Teencoders Techhub is a tech company that provides tech solutions to businesses and individuals</p>
+                  <h2 className='text-2xl'>{job.company.name}</h2>
+                  <p className='my-2'>{job.company.description}</p>
                   <hr className='my-4' />
                   <h3 className='text-xl'>Contact Email:</h3>
-                  <p className='my-2 p-2 font-bold bg-gray-100 rounded-lg'>teencoders@hub.com</p>
+                  <p className='my-2 p-2 font-bold bg-gray-100 rounded-lg'>{job.company.contactEmail}</p>
                   <h3 className='text-xl'>Contact Phone:</h3>
-                  <p className='my-2 p-2 font-bold bg-gray-100 rounded-lg'>08123456789</p>
+                  <p className='my-2 p-2 font-bold bg-gray-100 rounded-lg'>{job.company.contactPhone}</p>
                 </div>
 
                 <div className='bg-white p-6 mt-6 rounded-lg shadow-md'>
                   <h3 className='text-xl font-bold mb-6'>Manage Job</h3>
-                  <Link to='add-jobs' className='block text-center py-2 px-4 mt-4 w-full bg-gray-500 hover:bg-gray-700 text-white font-bold rounded-full focus:outline-none focus:shadow-outline'>Edit Job</Link>
+                  <Link to={`/jobs/edit/${job.id}`} className='block text-center py-2 px-4 mt-4 w-full bg-gray-500 hover:bg-gray-700 text-white font-bold rounded-full focus:outline-none focus:shadow-outline'>Edit Job</Link>
                   <button className='block py-2 px-4 text-center mt-4 bg-red-500 hover:bg-red-600 w-full text-white font-bold rounded-full focus:outline-none focus:shadow-outline'>Delete</button>
 
                 </div>
