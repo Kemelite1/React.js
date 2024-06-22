3bbb7de16ef8abb2c700fb7f03143de49de96405
kemelite1 <osikemelite42@gmail.com>
2024-06-22 22:04:54 +0100

    Form useState to manage form state use onChange to update form state

--- joblisting/src/pages/AddJobPage.jsx
+++ joblisting/src/pages/AddJobPage.jsx
@@ -1,14 +1,29 @@
+import { useState } from "react"
 
 const AddJobPage = () => {
+    const [title, setTitle] = useState('');
+    const [type, setType] = useState('Full-Time');
+    const [description, setDescription] = useState('');
+    const [salary, setSalary] = useState('$60K - $80K');
+    const [location, setLocation] = useState('');
+    const [companyName, setCompanyName] = useState('');
+    const [companyDescription, setCompanyDescription] = useState('');
+    const [contactEmail, setContactEmail] = useState('');
+    const [contactPhone, setContactPhone] = useState('');
+
+
+
+
   return (
     <section className="bg-green-50">
         <div className="container m-auto max-w-2xl py-24">
             <div className="bg-white py-8 px-6 m-4 mb-4 rounded-md shadow-md border md:m-0">
-                <form action="">
+                <form>
                     <h2 className="mb-6 text-3xl font-semibold text-center">Add Job</h2>
                     <div className="mb-4">
-                        <label htmlFor="title" className="block mb-2 font-bold text-gray-700">Job Type</label>
-                        <select name="type" id="type" className="py-2 px-3 border w-full rounded" required>
+                        <label htmlFor="type" className="block mb-2 font-bold text-gray-700">Job Type</label>
+                        <select name="type" id="type" className="py-2 px-3 border w-full rounded" value={type}
+                         onChange={(e) => setType(e.target.value)} required>
                             <option value="full-time">Full Time</option>
                             <option value="part-time">Part Time</option>
                             <option value="remote">Remote</option>
@@ -21,20 +36,23 @@ const AddJobPage = () => {
                     <div className="mb-4">
                         <label htmlFor="" className="block mb-2 text-gray-700 font-bold">Job Opening Name</label>
                         <input type="text" name="title" id="title" placeholder="Suite in Lagos" 
-                        className="py-2 px-3 mb-2 border w-full rounded"
+                        className="py-2 px-3 mb-2 border w-full rounded" value={title}
+                        onChange={(e) => setTitle(e.target.value)}
                          required />
                     </div>
 
                     <div className="mb-4">
                         <label htmlFor="description" className="block mb-2 text-gray-700 font-bold">Description</label>
                         <textarea name="description" id="description" placeholder="Make API calls to backend server" 
-                        className="py-2 px-3 border w-full rounded" rows='4'
+                        className="py-2 px-3 border w-full rounded" rows='4' value={description}
+                        onChange={(e) => setDescription(e.target.value)}
                         required></textarea>
                     </div>
 
                     <div className="mb-4">
                         <label htmlFor="salary" className="block mb-2 text-gray-700 font-bold">Salary</label>
-                        <select name="salary" id="salary" className="border w-full py-2 px-3 rounded">
+                        <select name="salary" id="salary" className="border w-full py-2 px-3 rounded" value={salary}
+                         onChange={(e) => setSalary(e.target.value)}>
                             <option value="$60K - $80K">$60K - $80K</option>
                             <option value="$80K - $100K">$80K - $100K</option>
                             <option value="$100K - $120K">$100K - $120K</option>
@@ -51,32 +69,38 @@ const AddJobPage = () => {
                     <div className="mb-4">
                         <label htmlFor="location" className="block mb-2 text-gray-700 font-bold">Location</label>
                         <input type="text" name="location" id="location" placeholder="Lagos, Nigeria"
-                         className="border w-full py-2 px-3 mb-2 rounded" required />
+                         className="border w-full py-2 px-3 mb-2 rounded" value={location}
+                         onChange={(e) => setLocation(e.target.value)} required />
                     </div>
 
                     <h3 className="mb-5 text-2xl">Company Info</h3>
                     <div className="mb-4">
                         <label htmlFor="companyName" className="block mb-2 text-gray-700 font-bold">Company Name</label>
                         <input type="text" name="company" id="company" placeholder="Google"
-                         className="border w-full py-2 px-3 mb-2 rounded" required />
+                         className="border w-full py-2 px-3 mb-2 rounded" value={companyName}
+                         onChange={(e) => setCompanyName(e.target.value)} required />
                     </div>
 
                     <div className="mb-4">
                         <label htmlFor="companyDescription" className="block mb-2 text-gray-700 font-bold">Company Description</label>
                         <textarea name="companyDescription" id="companyDescription" placeholder="We are a tech company" 
-                        className="border w-full py-2 px-3" rows='4' required></textarea>
+                        className="border w-full py-2 px-3" rows='4'
+                        value={companyDescription}
+                         onChange={(e) => setCompanyDescription(e.target.value)} required></textarea>
                     </div>
 
                     <div className="mb-4">
                         <label htmlFor="contactEmail" className="block mb-2 text-gray-700 font-bold">Contact Email</label>
                         <input type="email" name="contactEmail" id="contactEmail" placeholder="contact@google.com" 
-                        className="border w-full py-2 px-3 mb-2 rounded" required />
+                        className="border w-full py-2 px-3 mb-2 rounded" value={contactEmail}
+                        onChange={(e) => setContactEmail(e.target.value)} required />
                     </div>
 
                     <div className="mb-4">
                         <label htmlFor="contactPhone" className="block mb-2 text-gray-700 font-bold">Contact Phone</label>
                         <input type="tel" name="contactPhone" id="contactPhone" placeholder="+234 123 456 7890" 
-                        className="border w-full py-2 px-3 mb-2 rounded" required />
+                        className="border w-full py-2 px-3 mb-2 rounded" value={contactPhone}
+                        onChange={(e) => setContactPhone(e.target.value)} required />
                     </div>
 
                     <div className="mb-4">
